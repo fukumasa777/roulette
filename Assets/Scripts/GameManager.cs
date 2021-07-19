2b5593eca55ffa2fd33f2dae1d0a86b43362f44a
fukumasa777 <82266398+fukumasa777@users.noreply.github.com>
2021-07-19 15:01:40 +0900

    kousinn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,73 +3,104 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameManager: MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // 画像の生成
-    [SerializeField] Image imagePrefab = default;
-
-
-
+    [SerializeField] GameObject RouletteG = default;
     [SerializeField] Transform Roulette = default;
+    [SerializeField] Image imagePrefab2 = default;
+    [SerializeField] Image imagePrefab4 = default;
+    [SerializeField] Text resultText = default;
 
-    [SerializeField] Item itemPrefab = default;
-    [SerializeField] Transform setPanel = default;
 
     [SerializeField] GameObject setBG = default;
+    [SerializeField] Transform setPanel = default;
+    [SerializeField] Item itemPrefab = default;
+
 
     List<Item> itemList = new List<Item>();
+
     public float rotSpeed = 0;
 
-    [SerializeField] Arrow arrow = default;
 
     private void Start()
     {
         setBG.SetActive(false);
-
-        // 生成はできた
-        // ３つ配置
-        // 角度を120ずつずらす
-
         Debug.Log("スタート");
-        
-
-        
-
-        //List<ItemData> itemList = new List<ItemData>();
-        //itemList.Add(new ItemData(Color.red, "りんご", 1));
-        //itemList.Add(new ItemData(Color.yellow, "ばなな", 1));
-        //itemList.Add(new ItemData(Color.green, "もも", 1));
-
-
-
-        // Spawn(360/ 3 * 0, 2/4); => Spawn(0, 0.5f); 180ど埋まっている
-        // Spawn(360/ 3 * 1, 1/4); => Spawn(180, 0.25f); 180+90
-        // Spawn(360/ 3 * 1, 1/4); => Spawn(270, 0.25f);
-
-
-        //Spawn(120*0);
-        //Spawn(120*1);
-        //Spawn(120*2);
     }
 
-    public bool rouletteStart;
+    public bool isRouletteStart;
 
     private void Update()
     {
-        Roulette.transform.Rotate(0, 0, rotSpeed);
-        rotSpeed *= 0.99f;
-        if (rotSpeed >= -0.01f && rouletteStart)
+        if (isRouletteStart)
         {
-            rouletteStart = false;
-            Debug.Log("止まる" + arrow.targetName);
+            Roulette.transform.Rotate(0, 0, rotSpeed);//加速
+            rotSpeed *= 0.99f;//減速
+        }
+        if (rotSpeed >= -0.01f && isRouletteStart)
+        {
+            isRouletteStart = false;
+            Debug.Log("止まる");
+            result();
         }
     }
 
+    //回転
+    public void RotationBtn()
+    {
+        isRouletteStart = true;
+        rotSpeed = -1;
+    }
+
+    //結果表示
+    public void result()
+    {
+        if (itemList.Count == 2)
+        {
+            if (0 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180)
+            {
+                resultText.text = "結果 ： " + itemList[0].GetText();
+                Debug.Log(itemList[0].GetText());
+            }
+            if (180 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 360)
+            {
+                resultText.text = "結果 ： " + itemList[1].GetText();
+                Debug.Log(itemList[1].GetText());
+            }
+          
+        }
+        if (itemList.Count == 4)
+        {
+            if ( 0 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 90)
+            {
+                resultText.text = "結果 ： " + itemList[0].GetText();
+                Debug.Log(itemList[0].GetText());
+            }
+            if (270 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 360)
+            {
+                resultText.text = "結果 ： " + itemList[1].GetText();
+                Debug.Log(itemList[1].GetText());
+            }
+            if (180 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 270)
+            {
+                resultText.text ="結果 ： " + itemList[2].GetText();
+                Debug.Log(itemList[2].GetText());
+            }
+            if (90 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180)
+            {
+                resultText.text = "結果 ： " + itemList[3].GetText();
+                Debug.Log(itemList[0].GetText());
+            }
+        }
+    }
+
+    //項目追加画面表示
     public void SetMenuBtn()
     {
         setBG.SetActive(true);
     }
 
+    //項目追加しルーレット画面へ
     public void SetBtn()
     {
         setBG.SetActive(false);
@@ -82,60 +113,80 @@ public class GameManager: MonoBehaviour
         for (int i = 0; i < itemList.Count; i++)
         {
             float rate = itemList[i].rate / sumRate;
-            Debug.Log(itemList[i].rate);
-
-            Debug.Log(rate);
-
-            Spawn(nextAngle, rate, itemList[i].GetColor(), itemList[i].GetText());
-            nextAngle += 360 * rate;
+            // Spawn(nextAngle, rate, itemList[i].GetColor(), itemList[i].GetText());
             // Debug.Log(itemList[i].GetText());
+            if (itemList.Count == 2)
+            {
+                Spawn2(nextAngle, rate, itemList[i].GetColor(), itemList[i].GetText());
+
+            }
+            if (itemList.Count == 4)
+            {
+                Spawn4(nextAngle, rate, itemList[i].GetColor(), itemList[i].GetText());
+
+            }
+            nextAngle += 360 * rate;
         }
-
-        
     }
 
-    public void RotationBtn()
-    {
-        rotSpeed = -10;
-        rouletteStart = true;
-    }
+    
 
+   //項目追加
     public void ItemPlusBtn()
     {
-        Item item = Instantiate(itemPrefab,setPanel,false);
+        Item item = Instantiate(itemPrefab, setPanel, false);
         Color color = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f), 1);
         item.Set(color, 1);
-        // itemList.Add(new ItemData(Color.red, "りんご", 1));
         itemList.Add(item);
+        // itemList.Add(new ItemData(Color.red, "りんご", 1));
+    }
+
+    public void ItemResetBtn()
+    {
+        List<Item> itemList = new List<Item>();
+
     }
 
 
-    void Spawn(float angle, float rate ,Color color, string text)
+
+
+
+    //項目生成
+
+    /*原型
+    void Spawn(float angle, float rate, Color color, string text)
     {
-        Image image = Instantiate(imagePrefab, Roulette, false);
+        Image image = Instantiate(imagePrefab2, Roulette, false);
         image.transform.rotation = Quaternion.Euler(0, 0, angle);
         image.fillAmount = rate;
         image.color = color;
         //image.GetComponentInChildren<Text>().text = text;
     }
-
-    
-
-
-
-
-
+    */
+    void Spawn2(float angle, float rate, Color color, string text)
+    {
+        if (itemList.Count == 0)
+        {
+            return;
+        }
+        Image image = Instantiate(imagePrefab2, Roulette, false);
+        image.transform.rotation = Quaternion.Euler(0, 0, angle);
+        float angleDeg = Mathf.Deg2Rad * (180 / itemList.Count + angle);
+        image.transform.localPosition = new Vector3(Mathf.Sign(Mathf.Sin(angleDeg)) * 150,  0, 0);
+        image.color = color;
+        image.GetComponentInChildren<Text>().text = text;
+    }
+    void Spawn4(float angle, float rate, Color color, string text)
+    {
+        if (itemList.Count == 0)
+        {
+            return;
+        }
+        Image image = Instantiate(imagePrefab4, Roulette, false);
+        image.transform.rotation = Quaternion.Euler(0, 0, angle);
+        float angleDeg = Mathf.Deg2Rad * (180 / itemList.Count + angle);
+        image.transform.localPosition = new Vector3(Mathf.Sign(Mathf.Cos(angleDeg)) * 150, Mathf.Sign(Mathf.Sin(angleDeg)) * 150, 0);
+        image.color = color;
+        image.GetComponentInChildren<Text>().text = text;
+    }
 }
-
-//public class ItemData
-//{
-//    public Color color;
-//    public string itemName;
-//    public int rate;
-//    public ItemData(Color color, string itemName, int rate)
-//    {
-//        this.color = color;
-//        this.itemName = itemName;
-//        this.rate = rate;
-//    }
-//}
