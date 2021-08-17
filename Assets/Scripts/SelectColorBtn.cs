c2ff6defdc5ed52bf3a99e05c5c78c79c05c2c86
tanisu <walk.away.renee.5125@gmail.com>
2021-08-17 14:49:52 +0900

    color 関連

---
+++ Assets/Scripts/SelectColorBtn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SelectColorBtn : MonoBehaviour
+{
+    private Button button;
+    private Color myColor;
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Selected);
+        myColor = GetComponent<Image>().color;
+    }
+
+    private void Selected()
+    {
+        GameManager.I.SelectColorBtn(myColor);
+    }
+}
