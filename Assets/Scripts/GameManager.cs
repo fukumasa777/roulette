9b4c62edd8787d06980edf0185cc9ae549703450
tanisu <walk.away.renee.5125@gmail.com>
2021-08-18 16:13:13 +0900

    ルーレット基本形

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,45 +6,32 @@ using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject RouletteG = default;
-    [SerializeField] Transform Roulette = default;
-    [SerializeField] Image circlePrefab = default;
-    [SerializeField] Image imagePrefab2 = default;
-    [SerializeField] Image TitlePrefab = default;
-
-    [SerializeField] Image imagePrefab4 = default;
-    [SerializeField] Text resultText = default;
-
-
-    [SerializeField] GameObject setBG = default;
-    [SerializeField] GameObject colorPanel = default;
-    [SerializeField] GameObject colorPanelBtn = default;
-
-
-    [SerializeField] Transform setPanel = default;
-    [SerializeField] Item itemPrefab = default;
-
-
-    List<Item> itemList = new List<Item>();
-    List<GameObject> itemRouletteObjList = new List<GameObject>();
-    List<GameObject> titleList = new List<GameObject>();
-    List<float> angleResult = new List<float>();
-    List<Circle> circles = new List<Circle>();
-
-
-
-    List<Color> colorList = new List<Color>();
-    
-
-    public float rotSpeed = 0;
-    public float sumRate = 0;
-
-    //itemのボタン格納用変数（item側でクリックされたらこの中にgameobjectが入る）
-    public GameObject currentColorBtn;
 
     //参照出来るようにstatic変数用意
     public static GameManager I;
+    /*シリアライズフィールド達*/
+    [SerializeField] Transform Roulette = default;
+    [SerializeField] Image circlePrefab = default;
+    [SerializeField] Image TitlePrefab = default;    
+    [SerializeField] Text resultText = default;
+    [SerializeField] GameObject setBG = default;
+    [SerializeField] GameObject colorPanel = default;
+    [SerializeField] GameObject colorPanelBtn = default;
+    [SerializeField] Transform setPanel = default;
+    [SerializeField] Item itemPrefab = default;
 
+    /*List達*/
+    List<Item> itemList = new List<Item>();
+    List<GameObject> itemRouletteObjList = new List<GameObject>();
+    List<GameObject> titleList = new List<GameObject>();
+    List<Circle> circles = new List<Circle>();
+    List<Color> colorList = new List<Color>();
+    
+    /*public変数*/
+    public float rotSpeed = 0;
+    public float sumRate = 0;
+    public GameObject currentColorBtn; //itemのボタン格納用変数（item側でクリックされたらこの中にgameobjectが入る）
+    public bool isRouletteStart;
     //カラー用インデックス
     private int idx = 0;
 
@@ -60,7 +47,7 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         setBG.SetActive(false);
-        Debug.Log("スタート");
+        
         //カラーパレットの色をリストに追加
         foreach(Transform tf in colorPanelBtn.transform)
         {
@@ -69,7 +56,7 @@ public class GameManager : MonoBehaviour
 
     }
 
-    public bool isRouletteStart;
+    
 
     private void Update()
     {
@@ -81,7 +68,6 @@ public class GameManager : MonoBehaviour
         if (rotSpeed >= -0.01f && isRouletteStart)
         {
             isRouletteStart = false;
-            //Debug.Log("ストップ");
             result();
         }
     }
@@ -89,20 +75,18 @@ public class GameManager : MonoBehaviour
     //回転
     public void RotationBtn()
     {
-        //Debug.Log("回転");
         isRouletteStart = true;
-        rotSpeed = -1f;
+        rotSpeed = -5f;
     }
 
     
     //結果表示
     public void result()
     {
-        Debug.Log("総アイテム数"+itemList.Count);
+        /*Debug.Log("総アイテム数"+itemList.Count);
         Debug.Log($"サークル数:{circles.Count}");
-        Debug.Log("静止時アングル" + Roulette.transform.localEulerAngles.z);
+        Debug.Log("静止時アングル" + Roulette.transform.localEulerAngles.z);*/
         
-        //float resutAngle = 360 - Roulette.transform.localEulerAngles.z;
         float resutAngle = Roulette.transform.localEulerAngles.z;
         
         foreach(var (item, idx) in itemList.Select((item, idx)=> (item, idx)))
@@ -113,141 +97,6 @@ public class GameManager : MonoBehaviour
                 Debug.Log($"結果：{item.GetText()} / index:{idx}");
             }
         }
-        /*if (1 <= itemList.Count)
-        {
-            if (180 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[0])
-            {
-                resultText.text = "結果 ： " + itemList[0].GetText();
-                Debug.Log("結果 ： " + itemList[0].GetText());
-                Debug.Log(180 + angleResult[0]);
-            }
-            if (2 <= itemList.Count)
-            {
-                if (0 < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[1])
-                {
-                    resultText.text = "結果 ： " + itemList[1].GetText();
-                    Debug.Log("結果 ： " + itemList[1].GetText());
-                    Debug.Log(180 + angleResult[1]);
-                }
-                if (3 <= itemList.Count)
-                {
-                    if (180 + angleResult[1] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[2])
-                    {
-                        resultText.text = "結果 ： " + itemList[2].GetText();
-                        Debug.Log("結果 ： " + itemList[2].GetText());
-                        Debug.Log(180 + angleResult[2]);
-                    }
-                    if (4 <= itemList.Count)
-                    {
-                        if (180 + angleResult[2] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[3])
-                        {
-                            resultText.text = "結果 ： " + itemList[3].GetText();
-                            Debug.Log("結果 ： " + itemList[3].GetText());
-                            Debug.Log(180 + angleResult[3]);
-                        }
-                        if (5 <= itemList.Count)
-                        {
-                            if (180 + angleResult[3] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[4])
-                            {
-                                resultText.text = "結果 ： " + itemList[4].GetText();
-                                Debug.Log("結果 ： " + itemList[4].GetText());
-                                Debug.Log(180 + angleResult[4]);
-                            }
-                            if (6 <= itemList.Count)
-                            {
-                                if (180 + angleResult[4] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[5])
-                                {
-                                    resultText.text = "結果 ： " + itemList[5].GetText();
-                                    Debug.Log("結果 ： " + itemList[5].GetText());
-                                    Debug.Log(180 + angleResult[5]);
-                                }
-                                if (7 <= itemList.Count)
-                                {
-                                    if (180 + angleResult[5] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[6])
-                                    {
-                                        resultText.text = "結果 ： " + itemList[6].GetText();
-                                        Debug.Log("結果 ： " + itemList[6].GetText());
-                                        Debug.Log(180 + angleResult[6]);
-                                    }
-                                    if (8 <= itemList.Count)
-                                    {
-                                        if (180 + angleResult[6] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[7])
-                                        {
-                                            resultText.text = "結果 ： " + itemList[7].GetText();
-                                            Debug.Log("結果 ： " + itemList[7].GetText());
-                                            Debug.Log(180 + angleResult[7]);
-                                        }
-                                        if (9 <= itemList.Count)
-                                        {
-                                            if (180 + angleResult[7] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[8])
-                                            {
-                                                resultText.text = "結果 ： " + itemList[8].GetText();
-                                                Debug.Log("結果 ： " + itemList[8].GetText());
-                                                Debug.Log(180 + angleResult[8]);
-                                            }
-                                            if (10 <= itemList.Count)
-                                            {
-                                                if (180 + angleResult[8] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[9])
-                                                {
-                                                    resultText.text = "結果 ： " + itemList[9].GetText();
-                                                    Debug.Log("結果 ： " + itemList[9].GetText());
-                                                    Debug.Log(180 + angleResult[9]);
-                                                }
-                                                if (11 <= itemList.Count)
-                                                {
-                                                    if (180 + angleResult[9] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[10])
-                                                    {
-                                                        resultText.text = "結果 ： " + itemList[10].GetText();
-                                                        Debug.Log("結果 ： " + itemList[10].GetText());
-                                                        Debug.Log(180 + angleResult[10]);
-                                                    }
-                                                    if (12 <= itemList.Count)
-                                                    {
-                                                        if (180 + angleResult[10] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[11])
-                                                        {
-                                                            resultText.text = "結果 ： " + itemList[11].GetText();
-                                                            Debug.Log("結果 ： " + itemList[11].GetText());
-                                                            Debug.Log(180 + angleResult[11]);
-                                                        }
-                                                        if (13 <= itemList.Count)
-                                                        {
-                                                            if (180 + angleResult[11] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[12])
-                                                            {
-                                                                resultText.text = "結果 ： " + itemList[12].GetText();
-                                                                Debug.Log("結果 ： " + itemList[12].GetText());
-                                                                Debug.Log(180 + angleResult[12]);
-                                                            }
-                                                            if (14 <= itemList.Count)
-                                                            {
-                                                                if (180 + angleResult[12] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[13])
-                                                                {
-                                                                    resultText.text = "結果 ： " + itemList[13].GetText();
-                                                                    Debug.Log("結果 ： " + itemList[13].GetText());
-                                                                    Debug.Log(180 + angleResult[13]);
-                                                                }
-                                                                if (15 <= itemList.Count)
-                                                                {
-                                                                    if (180 + angleResult[13] < RouletteG.transform.localEulerAngles.z && Roulette.transform.localEulerAngles.z <= 180 + angleResult[14])
-                                                                    {
-                                                                        resultText.text = "結果 ： " + itemList[14].GetText();
-                                                                        Debug.Log("結果 ： " + itemList[14].GetText());
-                                                                        Debug.Log(180 + angleResult[14]);
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }*/
     }
 
     //項目追加画面表示
@@ -294,36 +143,19 @@ public class GameManager : MonoBehaviour
         {
             sumRate += itemList[i].rate;
         }
-        float startAngle = 0;
-        float endAngle = 0;
-        float nextTitleAngle = (360f * itemList[0].rate / sumRate) / 2f;
+
+        float startAngle = 0;//スタートの角度
+        float endAngle = 0;//終了の角度
         for (int i = 0; i < itemList.Count; i++)
         {
             float rate = itemList[i].rate / sumRate;
-            //Spawn(startAngle, rate, itemList[i].GetColor(), itemList[i].GetText());
-            
-            /*一旦非表示*/
-            //TitleSpawn(nextTitleAngle, rate, itemList[i].GetText());
-            
+
             endAngle += 360 * rate;
-            nextTitleAngle += 360f * rate;
-            angleResult.Add(endAngle);
-            
+
             Spawn(startAngle,endAngle, rate, itemList[i].GetColor(), itemList[i].GetText());
+            TitleSpawn(itemList[i].GetText());
             //次のスタートアングルのためにendAngleをstartAngleに代入
             startAngle = endAngle;
-
-
-            //if(nextAngle <= 180)
-            //{
-            //    angleResult.Add(nextAngle);
-            //}
-            //else
-            //{
-            //    angleResult.Add(nextAngle-360);
-            //}
-
-
         }
     }
 
@@ -345,6 +177,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //ルーレット項目リセット処理
     public void ItemResetBtn()
     {
         //カラー呼び出し用indexを初期化
@@ -382,23 +215,7 @@ public class GameManager : MonoBehaviour
 
     }
 
-
-
-    //項目生成
-
-
-
-    /*原型
-    void Spawn(float angle, float rate, Color color, string text)
-    {
-        Image image = Instantiate(imagePrefab2, Roulette, false);
-        image.transform.rotation = Quaternion.Euler(0, 0, angle);
-        image.fillAmount = rate;
-        image.color = color;
-        //image.GetComponentInChildren<Text>().text = text;
-    }
-    */
-
+    //円生成
     void Spawn(float startAngle,float endAngle, float rate, Color color, string text)
     {
         Image image = Instantiate(circlePrefab, Roulette, false);
@@ -407,65 +224,26 @@ public class GameManager : MonoBehaviour
         circles.Add(image.GetComponent<Circle>());
         circles.Last().startAngle = startAngle;
         circles.Last().endAngle = endAngle;
-
-        
-        
         image.color = color;
         itemRouletteObjList.Add(image.gameObject);
     }
 
-    void TitleSpawn(float angle, float rate, string text)
+    //各円の見出し生成
+    void TitleSpawn( string text)
     {
         if (itemList.Count == 0)
         {
             return;
         }
         Image image = Instantiate(TitlePrefab, Roulette, false);
-        float angleDeg = Mathf.Deg2Rad * (angle);   
-        // Debug.Log($"angleDeg{angleDeg}");
-        int k = -200;
+        float angleDeg = Mathf.Deg2Rad * ((circles.Last().startAngle + circles.Last().endAngle) / 2);
+        int k = 200;//円中心からの何か
         image.transform.localPosition = new Vector3(k * Mathf.Sin(angleDeg), k * Mathf.Cos(angleDeg), 0);
         image.GetComponentInChildren<Text>().text = text;
         titleList.Add(image.gameObject);
     }
 
-    void Spawn2(float angle, float rate, Color color, string text)
-    {
-        Debug.Log(rate);
-        if (itemList.Count == 0)
-        {
-            return;
-        }
-        Image image = Instantiate(imagePrefab2, Roulette, false);
-        image.transform.rotation = Quaternion.Euler(0, 0, angle);
-        float angleDeg = Mathf.Deg2Rad * (angle);
-        image.transform.localPosition = new Vector3(GetSign(Mathf.Cos(angleDeg)) * 150, GetSign(Mathf.Sin(angleDeg)) * 150, 0);
-        image.color = color;
-        image.GetComponentInChildren<Text>().text = text;
-    }
+ 
 
-    float GetSign(float value)
-    {
-        if (Mathf.Abs(value) < 0.0001f)
-        {
-            return 0;
-        }
-        return Mathf.Sign(value);
-    }
-    void Spawn4(float angle, float rate, Color color, string text)
-    {
-        Debug.Log(rate);
-
-        if (itemList.Count == 0)
-        {
-            return;
-        }
-        Image image = Instantiate(imagePrefab4, Roulette, false);
-        image.transform.rotation = Quaternion.Euler(0, 0, angle);
-        float angleDeg = Mathf.Deg2Rad * (180 / itemList.Count + angle);
-        // image.transform.localPosition = new Vector3(GetSign(Mathf.Cos(angleDeg)) * 150, GetSign(Mathf.Sin(angleDeg)) * 150, 0);
-        image.transform.localPosition = new Vector3(Mathf.Sign(Mathf.Cos(angleDeg)) * 150, Mathf.Sign(Mathf.Sin(angleDeg)) * 150, 0);
-        image.color = color;
-        image.GetComponentInChildren<Text>().text = text;
-    }
+ 
 }
