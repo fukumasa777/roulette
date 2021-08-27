1737139bca262f0c4f924d6d30e201759b7d23c9
fukumasa777 <82266398+fukumasa777@users.noreply.github.com>
2021-08-28 00:12:57 +0900

    アドモブ実装

---
+++ Assets/Scripts/GoogleMobileAdsDemoScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Placement;
+
+public class MainScene : MonoBehaviour
+{
+    InterstitialAdGameObject interstitialAd;
+
+    void Start()
+    {
+        interstitialAd = MobileAds.Instance
+            .GetAd<InterstitialAdGameObject>("interstitial");
+
+        MobileAds.Initialize((initStatus) => {
+            Debug.Log("Initialized MobileAds");
+        });
+    }
+
+    public void OnClickShowGameSceneButton()
+    {
+        // Display an interstitial ad
+        interstitialAd.ShowIfLoaded();
+
+        // Load a scene named "GameScene"
+        SceneManager.LoadScene("GameScene");
+    }
+}
---
+++ Assets/Scripts/MScene.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Placement;
+
+public class MScene : MonoBehaviour
+{
+    InterstitialAdGameObject interstitialAd;
+    RewardedAdGameObject rewardedAdGameObject;
+    void Start()
+    {
+
+        rewardedAdGameObject = MobileAds.Instance.GetAd<RewardedAdGameObject>("TestRewarded Ad");
+
+        interstitialAd = MobileAds.Instance.GetAd<InterstitialAdGameObject>("Interstitial Ad");
+
+        MobileAds.Initialize((initStatus) =>
+        {
+            Debug.Log("Initialized MobileAds");
+        });
+        //interstitialAd.LoadAd();
+        rewardedAdGameObject.LoadAd();
+    }
+
+    public void OnClickShowSceneButton()
+    {
+        // Display an interstitial ad
+        //interstitialAd.ShowIfLoaded();
+        rewardedAdGameObject.ShowIfLoaded();
+
+    }
+
+    /*
+    public void OnRewardCloseBtn()
+    {
+        Debug.Log("動画をみた");
+    }
+    */
+}
